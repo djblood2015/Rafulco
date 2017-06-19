d37aaef9f8f7e1973a5b954b0a7ef5607c2de811
cesar calbucan <ciber06.11@gmail.com>
2017-06-19 15:02:24 -0400

    cambios
    rafulco site

---
+++ Mapas02.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2363.322117580814!2d-73.13363498508336!3d-40.58349130528698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9616367cfba6f52f%3A0x37fd36f0b7ec7449!2sEstadio+Parque+Schott!5e1!3m2!1ses-419!2scl!4v1448671756686" width="420" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+</body>
